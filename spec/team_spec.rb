1b3ed6323684b7a341580047360f03cc4e709242
KaraJH <karas.jones@yahoo.com>
2022-10-24 16:43:40 -0600

    feat: Adds the tests for short term and long term methods

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -27,6 +27,44 @@ RSpec.describe Team do
       player_2 = Player.new("Kenny DeNunez", 500000, 24)
       team.add_player(player_1)
       expect(player_1).to be_a(Player)
+
+      team.add_player(player_2)
+      expect(player_2).to be_a(Player)
+
+      expect(team.roster).to eq([player_1, player_2])
+    end
+  end
+
+  describe '#long_term_players' do
+    it 'checks who is a long term vs short term contract player' do
+      team = Team.new("Dodgers", "Los Angeles")
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+      team.roster
+
+      expect(team.long_term_players).to eq([player_1, player_3])
+    end
+  end
+  describe '#short_term_players' do
+    it 'checks who is a long term vs short term contract player' do
+      team = Team.new("Dodgers", "Los Angeles")
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+      team.roster
+
+      expect(team.short_term_players).to eq([player_2, player_4])
     end
   end
 end

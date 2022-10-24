9626576d99c7f1e469fdfe3483c37d969ffde532
KaraJH <karas.jones@yahoo.com>
2022-10-24 16:55:48 -0600

    This adds the passing test for the total cost

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -51,6 +51,7 @@ RSpec.describe Team do
       expect(team.long_term_players).to eq([player_1, player_3])
     end
   end
+
   describe '#short_term_players' do
     it 'checks who is a long term vs short term contract player' do
       team = Team.new("Dodgers", "Los Angeles")
@@ -67,4 +68,22 @@ RSpec.describe Team do
       expect(team.short_term_players).to eq([player_2, player_4])
     end
   end
+
+  describe '#total_value' do
+    it 'calculates the cost of all the players' do
+      team = Team.new("Dodgers", "Los Angeles")
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+      team.roster
+
+      expect(team.total_value).to eq(85200000)
+    end
+  end
+
 end

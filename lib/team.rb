da7828cecea90cde579af1ea4fa522b8015e5a64
KaraJH <karas.jones@yahoo.com>
2022-10-24 16:43:16 -0600

    feat: Added the long term and short term methods

--- lib/team.rb
+++ lib/team.rb
@@ -1,7 +1,7 @@
 
 class Team
   attr_reader :team_name, :city,
-              :roster, :player_count
+              :player_count, :roster
 
   def initialize(team_name, city)
     @team_name = team_name
@@ -10,6 +10,48 @@ class Team
     @player_count = 0
   end
 
+
   def add_player(player)
+    @roster << player
+    #require 'pry' ;binding.pry
+    @player_count += 1
   end
+
+  def long_term_players
+    long_term = []
+    roster.select do |player|
+
+      if player.contract_length > 24
+        long_term << player
+      end
+    end
+  end
+
+  def short_term_players
+    short_term = []
+    roster.select do |player|
+
+      if player.contract_length <= 24
+        short_term << player
+      end
+    end
+  end
+
+  # def contract_info
+  #
+  #
+  #   @roster.map do |player|
+  #     @long_term_players = []
+  #
+  #     @short_term_players = []
+  #
+  #     if player.contract_length > 24
+  #       @long_term_players << player
+  #     elsif player.contract_length <= 24
+  #       @short_term_players << player
+  #     end
+  #   end
+  #
+  # end
+
 end

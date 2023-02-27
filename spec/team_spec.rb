b1172a1670632e1bfb9ab22b47152b871a7717d2
KaraJH <karas.jones@yahoo.com>
2023-02-26 18:03:16 -0700

    test: Long term player method

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Team do
     @team = Team.new("Dodgers", "Los Angeles")
     @player_1 = Player.new("Michael Palledorous" , 1000000, 36)
     @player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    @player_3 = Player.new("Alan McClennan", 750000, 48)
+    @player_4 = Player.new("Hamilton Porter", 100000, 12)
   end
   describe 'initialize' do 
     it 'exists and has attributes' do 
@@ -23,8 +25,20 @@ RSpec.describe Team do
 
       @team.add_player(@player_1)
       @team.add_player(@player_2)
-      
+
       expect(@team.roster).to eq([@player_1, @player_2])
+      expect(@team.player_count).to eq(2)
+    end
+  end
+
+  describe 'long_term_players' do 
+    it 'is a long term player if their contract length is greater than 2 years' do 
+      @team.add_player(@player_1)
+      @team.add_player(@player_2)
+      @team.add_player(@player_3)
+      @team.add_player(@player_4)
+
+      expect(@team.long_term_players).to eq([@player_1, @player_3])
     end
   end
 end

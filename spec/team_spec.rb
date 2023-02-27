6a91a074f068df9c008a12f60a8665fe05ae5e5a
KaraJH <karas.jones@yahoo.com>
2023-02-26 17:58:17 -0700

    test: Add player test

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -4,6 +4,8 @@ require 'rspec'
 RSpec.describe Team do 
   before(:each) do 
     @team = Team.new("Dodgers", "Los Angeles")
+    @player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    @player_2 = Player.new("Kenny DeNunez", 500000, 24)
   end
   describe 'initialize' do 
     it 'exists and has attributes' do 
@@ -12,6 +14,17 @@ RSpec.describe Team do
       expect(@team.city).to eq("Los Angeles")
       expect(@team.roster).to eq([])
       expect(@team.player_count).to eq(0)
+    end
+  end
+
+  describe '#add_player' do 
+    it 'adds a player to the roster' do 
+      expect(@team.roster).to eq([])
+
+      @team.add_player(@player_1)
+      @team.add_player(@player_2)
+      
+      expect(@team.roster).to eq([@player_1, @player_2])
     end
   end
 end
